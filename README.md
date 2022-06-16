4597311b9a736e2c0631caeae8e877030df79772
Jari O. O. Pennanen <ciantic@oksidi.com>
2022-06-16 20:40:38 +0300

    ...

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# foo
+# Hello world with emscripten and Rust
+
+```
+git clone https://github.com/emscripten-core/emsdk ~/emsdk
+~/emsdk install tot
+~/emsdk activate tot
+source ~/emsdk/emsdk_env.sh
+cargo build --target wasm32-unknown-emscripten
+```
--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
 fn main() {
-    
     println!("cargo:rustc-link-arg=--no-entry");
     
     // THIS DOES NOT WORK, because something sets in CLI it already:
@@ -8,9 +7,7 @@ fn main() {
     // THIS WORKS!
     println!("cargo:rustc-env=EMCC_CFLAGS=-s ERROR_ON_UNDEFINED_SYMBOLS=0");
     
-    
     cc::Build::new()
-    
         .file("src/test.c")
         .compile("test");
 }
