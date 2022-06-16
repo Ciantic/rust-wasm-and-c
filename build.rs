8706857da7b199539722a1b8b66eee6b66fac301
Jari O. O. Pennanen <ciantic@oksidi.com>
2022-06-16 22:24:03 +0300

    for completeness produce also JS file

--- build.rs
+++ build.rs
@@ -5,6 +5,9 @@ fn main() {
         && std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "emscripten"
     {
         println!("cargo:rustc-link-arg=--no-entry");
+        println!("cargo:rustc-link-arg=-s EXPORT_ES6=1");
+        println!("cargo:rustc-link-arg=-s MODULARIZE=1");
+        println!("cargo:rustc-link-arg=-o module.html");
 
         // THIS DOES NOT WORK, because something sets in CLI it already:
         // println!("cargo:rustc-link-arg=-sERROR_ON_UNDEFINED_SYMBOLS=0");
